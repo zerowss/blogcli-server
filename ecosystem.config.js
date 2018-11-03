8394bd815552f03b554ca6be251e517b91c7110e
wangsongsong <wangsongsong@meiliwu.com>
2018-11-03 17:47:39 +0800

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: wangss 
+ * @Date: 2018-11-03 14:23:32 
+ * @Last Modified by: wangss
+ * @Last Modified time: 2018-11-03 14:58:48
+ */
+
+module.exports = {
+    apps:[
+        {
+            name: "blg-app",
+            script: './app.js',
+            watch: true,
+        }
+    ]
+}; 
