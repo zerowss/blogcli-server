a6a96f242808d407202ef238eb0132581a3bf1c1
wangsongsong <wangsongsong@meiliwu.com>
2018-11-05 15:59:51 +0800

    add pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,26 @@
-/*
- * @Author: wangss 
- * @Date: 2018-11-03 14:23:32 
- * @Last Modified by: wangss
- * @Last Modified time: 2018-11-03 14:58:48
- */
-
 module.exports = {
-    apps:[
-        {
-            name: "blg-app",
-            script: './app.js',
-            watch: true,
-        }
-    ]
-}; 
+  apps : [{
+    name: 'API',
+    script: 'app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    autorestart: true,
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '47.104.165.117',
+      ref  : 'origin/master',
+      repo : 'git@github.com:zerowss/blogcli-server.git',
+      path : '/workspace/blgcli-server',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
