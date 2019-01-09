5b421a2db1fd793f9270a46d363190de5428c5f9
wangsongsong <wangsongsong@meiliwu.com>
2019-01-09 14:56:26 +0800

    add tkoen

--- src/routers/api/user_router.js
+++ src/routers/api/user_router.js
@@ -2,7 +2,7 @@
  * @Author: wangss 
  * @Date: 2018-11-02 15:42:55 
  * @Last Modified by: wangss
- * @Last Modified time: 2019-01-09 14:49:59
+ * @Last Modified time: 2019-01-09 14:56:03
  */
 
 const Router = require('koa-router');
@@ -16,7 +16,7 @@ router.post('/login', user_controller.login)
       .post('/register', user_controller.register)
       .post('/getEmailCode', user_controller.getEmailCode)
       .get('/isSameName', user_controller.isSameName)
-      .get('/checkToken', weChat_controller.checkToken)
+      .get('/check-token', weChat_controller.checkToken)
       .get('/wechat/redirect', weChat_controller.weChatLogin)
       .get('/wechat/getToken',weChat_controller.get_wx_access_token);
 
